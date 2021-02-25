ba8ee5c76a0c2b473da362f09b96756dcca9831e
Banin Sensha Shrestha <baninsenshashrestha@Banins-MacBook-Pro.local>
2021-02-25 16:41:49 +0545

    css implemented when category is selected

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import classNames from 'classnames';
 
 const CategoryList = ({ categoryList, setCategoryList, divRef}) => {
+    const [textSelected, setTextSelected] = useState({0: true});
 
     useEffect(() => {
         axios.get("http://localhost:5000/categories/")
@@ -12,6 +14,7 @@ const CategoryList = ({ categoryList, setCategoryList, divRef}) => {
 
     const scrollToView = (e, index) => {
         e.preventDefault();
+        setTextSelected({[index]: true}); 
         divRef.current[index].scrollIntoView({
             behavior: 'smooth',
             block: 'start',
@@ -24,7 +27,9 @@ const CategoryList = ({ categoryList, setCategoryList, divRef}) => {
                 (categoryList || []).map((category, index) => {
                     return (
                         <div
-                            className="category-name curP mb-10x font-medium fs-15" 
+                            className={classNames("category-name curP mb-10x font-medium fs-15", {
+                                'text-selected': textSelected[index] 
+                            })}
                             key={index}
                             onClick={(e) => scrollToView(e,index)}
                         >
